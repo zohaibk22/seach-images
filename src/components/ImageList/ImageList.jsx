e60bd81d7f8d9b33e41628ce2ce408b42bbf0a26
Zohaib Khan <zohaibk1111@gmail.com>
2021-07-15 00:25:15 -0400

    V1 of our app is done

--- src/components/ImageList/ImageList.jsx
+++ src/components/ImageList/ImageList.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
 const ImageList = (props) =>{
+    const images = props.images.map(({description, id, urls})=>{
+        return <img alt={description} key={id}src={urls.regular}></img>
+    })
     return(
         <>
-        {props.images.map((image)=>(
-            <div>
-                <img src={image.urls.regular}></img>
-            </div>
-        ))}
+        {images}
         </>
     )
 }
