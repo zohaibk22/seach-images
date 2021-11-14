ebb489bd6191c94d5c669c229e11cbb853dafee0
Zohaib Khan <zohaibk1111@gmail.com>
2021-11-14 01:07:33 -0500

    app done

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -6,17 +6,33 @@ class ImageCard extends Component {
 
         this.imageRef = React.createRef()
 
+        this.state = {
+            spans: 0,
+        }
+
     }
 
     componentDidMount(){
-        console.log(this.imageRef.current.clientHeight)
+        this.imageRef.current.addEventListener('load', this.setSpans)
+    }
+
+    setSpans = () =>{
+        console.log(this.imageRef.current.clientHeight, "Zohaib")
+        const height = this.imageRef.current.clientHeight
+        
+        const spans = Math.ceil(height/ 10)
+
+        // this.setState({spans: spans}) 
+        // es2015 way to do this
+        this.setState({spans})
+
     }
     render(){
         const {alt_description, urls} = this.props.image
         console.log(this.props.image)
         return (
             <>
-                <img 
+                <img style={{gridRowEnd: `span ${this.state.spans}`}}
                     ref={this.imageRef}
                     alt={alt_description}
                     src={urls.regular}
